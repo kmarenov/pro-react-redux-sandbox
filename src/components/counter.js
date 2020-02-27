2f8e6fe4a971f578a78f1eb822de37276f771119
Kirill Marenov <kmarenov@gmail.com>
2020-02-27 15:23:41 +0300

    126. Функция mapDispatchToProps()

---
+++ src/components/counter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+const Counter = ({counter, inc, dec, rnd}) => {
+    return (
+        <div className="jumbotron">
+            <h2>{counter}</h2>
+            <button className="btn btn-primary btn-lg" onClick={dec}>DEC</button>
+            <button className="btn btn-primary btn-lg" onClick={inc}>INC</button>
+            <button className="btn btn-primary btn-lg" onClick={rnd}>RND</button>
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+    return {
+        inc,
+        dec,
+        rnd: () => {
+            const randomValue = Math.floor(Math.random()*10);
+            rnd(randomValue);
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
-import { createStore, bindActionCreators } from 'redux';
+import { createStore } from 'redux';
 import reducer from './reducer';
-import * as actions from './actions';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './counter';
+import { Provider } from 'react-redux';
+import App from './components/app';
 
 const store = createStore(reducer);
-const { dispatch } = store;
 
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-const update = () => {
-    ReactDOM.render(
-        <Counter
-            counter={store.getState()}
-            inc={inc}
-            dec={dec}
-            rnd={() => {
-                const value = Math.floor(Math.random()*10);
-                rnd(value);
-            }}
-        />,
-        document.getElementById('root')
-    );
-};
-
-update();
-store.subscribe(update);
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+);
